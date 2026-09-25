34706d805d0ea85e2ffc43b914bc74ff8e38f1ab
Rajlaxmi Patil <rajdpatil.13@gmail.com>
2026-09-25 12:29:25 +0530

    Day 1: Practice Python data types

---
+++ 01_Python_Basics/datatypes.py
@@ -0,0 +1,38 @@
+# Python Data Types
+
+# Integer
+a = 10
+
+# Float
+b = 10.5
+
+# String
+name = "Python"
+
+# Boolean
+is_valid = True
+
+# List
+numbers = [10, 20, 30, 40]
+
+# Tuple
+values = (10, 20, 30)
+
+# Set
+unique_numbers = {10, 20, 30}
+
+# Dictionary
+student = {
+    "name": "Raj",
+    "age": 21,
+    "course": "BCA"
+}
+
+print(a, type(a))
+print(b, type(b))
+print(name, type(name))
+print(is_valid, type(is_valid))
+print(numbers, type(numbers))
+print(values, type(values))
+print(unique_numbers, type(unique_numbers))
+print(student, type(student))
