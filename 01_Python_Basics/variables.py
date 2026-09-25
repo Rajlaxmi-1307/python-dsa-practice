204d2b1afa5570a1af021c6365bd49d7855eea54
Rajlaxmi Patil <rajdpatil.13@gmail.com>
2026-09-25 11:39:08 +0530

    Add variables.py with basic variable examples

---
+++ 01_Python_Basics/variables.py
@@ -0,0 +1,25 @@
+# Python Variables
+
+name = "Raj"
+age = 21
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Multiple variable assignment
+a, b, c = 10, 20, 30
+
+print("A:", a)
+print("B:", b)
+print("C:", c)
+
+# Swapping two variables
+a, b = b, a
+
+print("After swapping:")
+print("A:", a)
+print("B:", b)
